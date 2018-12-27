7fc3c42b165ddf52cb7896394d95a9c445688da7
Cuong Kieu <kieucuong01@gmail.com>
2018-12-27 16:16:52 +0700

    + First commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+end
---
+++ config/initializers/assets.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( theme.css )
+Rails.application.config.assets.precompile += %w( theme-elements.css )
+Rails.application.config.assets.precompile += %w( theme-shop.css )
+Rails.application.config.assets.precompile += %w( theme-blog.css )
+Rails.application.config.assets.precompile += %w( skins/skin-resume.css )
+Rails.application.config.assets.precompile += %w( demos/demo-resume.css )
+Rails.application.config.assets.precompile += %w( custom.css )
+
+Rails.application.config.assets.precompile += %w( theme.js )
+Rails.application.config.assets.precompile += %w( views/view.contact.js )
+Rails.application.config.assets.precompile += %w( demos/demo-resume.js )
+Rails.application.config.assets.precompile += %w( custom.js )
+Rails.application.config.assets.precompile += %w( theme.init.js )
